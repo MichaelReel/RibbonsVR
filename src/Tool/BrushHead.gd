6ffd5e0400a2b28d2c15a13ed78784ce995176b6
Michael Reel <mykilr@gmail.com>
2020-01-11 21:59:48 +0000

    Pushing for the night, need to figure out the drawing code.

--- src/Tool/BrushHead.gd
+++ src/Tool/BrushHead.gd
@@ -12,11 +12,19 @@ const min_size = Vector2(0.01, 0.01)
 const max_size = Vector2(2.0, 2.0)
 const edit_speed = Vector2(0.1, 0.1)
 
+#var previous_line : AABB = null
+
 func _process(delta):
 	
 	if controller == null:
 		controller = brush.get_controller()
 		return
+	
+	update_brush_size(delta)
+	
+	draw_ribbon(delta)
+
+func update_brush_size(delta):
 	
 	var new_size = plane_mesh.get_size()
 	
@@ -24,13 +32,22 @@ func _process(delta):
 		new_size.x += controller.get_joystick_axis(0) * delta * edit_speed.x
 		new_size.x = max(min_size.x, new_size.x)
 		new_size.x = min(max_size.x, new_size.x)
-		
+	
 	if controller.get_joystick_axis(1):
 		new_size.y += controller.get_joystick_axis(1) * delta * edit_speed.y
 		new_size.y = max(min_size.y, new_size.y)
 		new_size.y = min(max_size.y, new_size.y)
-		
+	
 	if new_size: 
 		plane_mesh.set_size(new_size)
 		translation.z = z_offset - new_size.y / 2
-	
+
+func draw_ribbon(delta):
+	if controller.is_button_pressed(15):
+		# TODO: Need to start a drawing mesh when none existent
+		#       Make the ribbon section as the controller is moved
+		#       Finalise sections when the controller is moved far enough
+		#       May need some kind a singleton service to manage the drawing
+		
+		pass
+			
