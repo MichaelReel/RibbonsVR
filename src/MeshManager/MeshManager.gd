74bba85985a9965be7d3a698c266bb9e0313eaf7
Michael Reel <mykilr@gmail.com>
2020-01-16 22:02:32 +0000

    Fixed the ribbon drawing line rotations. Added some temp debug. shrunk debug orb.

--- src/MeshManager/MeshManager.gd
+++ src/MeshManager/MeshManager.gd
@@ -1,6 +1,6 @@
 extends Spatial
 
-onready var debug_mesh = $MeshInstance
+onready var debug_orb_mesh = $MeshInstance
 
 var surf_tool_front : SurfaceTool = null
 var surf_tool_back : SurfaceTool = null
@@ -12,8 +12,27 @@ var mesh_instance_back : MeshInstance = null
 var last_line : AABB = AABB()
 
 func brush_stroke_started(position, draw_head):
-#	debug_orb_thing(position, draw_head)
+	begin_new_ribbon_surface(draw_head)
 	
+#	debug_drawing_line(draw_head)
+#	debug_orb_thing(position)
+#	debug_clone_brush(draw_head)
+
+func brush_segment(position, draw_head):
+	update_strips(get_current_line(draw_head))
+	
+#	debug_drawing_line(draw_head)
+#	debug_orb_thing(position)
+#	debug_clone_brush(draw_head)
+
+func brush_stroke_ended(position, draw_head):
+	finish_current_ribbon_surface(draw_head)
+	
+#	debug_drawing_line(draw_head)
+#	debug_orb_thing(position)
+#	debug_clone_brush(draw_head)
+
+func begin_new_ribbon_surface(draw_head : MeshInstance):
 	surf_tool_front = SurfaceTool.new()
 	surf_tool_back = SurfaceTool.new()
 	mesh_front = Mesh.new()
@@ -36,15 +55,8 @@ func brush_stroke_started(position, draw_head):
 	self.add_child(mesh_instance_back)
 	mesh_instance_front.set_owner(self)
 	mesh_instance_back.set_owner(self)
-	
 
-func brush_segment(position, draw_head):
-#	debug_orb_thing(position, draw_head)
-	update_strips(get_current_line(draw_head))
-	
-
-func brush_stroke_ended(position, draw_head):
-#	debug_orb_thing(position, draw_head)
+func finish_current_ribbon_surface(draw_head : MeshInstance):
 	update_strips(get_current_line(draw_head))
 	
 	self.add_child(mesh_instance_front)
@@ -59,35 +71,15 @@ func brush_stroke_ended(position, draw_head):
 	mesh_instance_front = null
 	mesh_instance_back = null
 
-func debug_orb_thing(position, draw_head):
-	print(str(position), ", ", str(draw_head))
-
-	var mesh_inst : MeshInstance = draw_head
-	var verts : PoolVector3Array = mesh_inst.mesh.get_faces()
-
-#	var debug_face_1 : MeshInstance = mesh_inst.duplicate(0)
-	var debug_face_1 : MeshInstance = debug_mesh.duplicate(0)
-
-	self.add_child(debug_face_1)
-	debug_face_1.set_owner(self)
-	debug_face_1.set_translation(position)
-	debug_face_1.transform = mesh_inst.get_global_transform()
-	
 func get_current_line(draw_head) -> AABB:
 	var trans : Transform = draw_head.get_global_transform()
-	var mesh_inst : MeshInstance = draw_head.duplicate(0)
-	var verts : PoolVector3Array = mesh_inst.mesh.get_faces()
-	
-	
 	var brush_size : PlaneMesh = draw_head.mesh.get_size()
-	var corner : Vector3 = Vector3(-brush_size.x / 2.0, 0, brush_size.y / 2.0)
-	var width : Vector3 = Vector3(brush_size.x, 0, 0)
-	
+	var corner : Vector3 = Vector3(brush_size.x / 2.0, 0, brush_size.y / 2.0)
+	var width : Vector3 = Vector3(-brush_size.x, 0, 0)
 	
 	# Get the 2 vertices closest to the tool as the draw line, rotate and translate to the world coords
-	var aabb : AABB = AABB(corner, width)
-	return trans.xform(aabb)
-	
+	return AABB(trans.basis.xform(corner) + trans.origin, trans.basis.xform(width))
+
 func update_strips(line : AABB):
 	
 	# A-----B    A: last_line.position
@@ -123,4 +115,30 @@ func update_strips(line : AABB):
 	mesh_instance_back.set_mesh(mesh_back)
 	
 	last_line = line
+
+# Debugging Functions
+
+func debug_orb_thing(position):
+	var debug_face_1 : MeshInstance = debug_orb_mesh.duplicate(0)
+	self.add_child(debug_face_1)
+	debug_face_1.set_owner(self)
+	debug_face_1.set_translation(position)
+
+func debug_clone_brush(draw_head : MeshInstance):
+	var mesh_inst : MeshInstance = MeshInstance.new()
+	var mesh_back : MeshInstance = MeshInstance.new()
+	self.add_child(mesh_inst)
+	self.add_child(mesh_back)
+	mesh_inst.set_mesh(draw_head.mesh.duplicate(0))
+	mesh_back.set_mesh(draw_head.mesh.duplicate(0))
+	mesh_inst.set_owner(self)
+	mesh_back.set_owner(self)
+	mesh_inst.set_transform(draw_head.get_global_transform())
+	mesh_back.set_transform(draw_head.get_global_transform())
+	mesh_back.mesh.set_flip_faces(true)
+
+func debug_drawing_line(draw_head : MeshInstance):
+	var line : AABB = get_current_line(draw_head)
+	debug_orb_thing(line.position)
+	debug_orb_thing(line.position + line.size)
 	
